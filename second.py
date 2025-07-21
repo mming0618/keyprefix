14acaa7098c9830876d9cad5f65d172bc67e5558
mming0618 <126595607+mming0618@users.noreply.github.com>
2025-07-22 04:58:39 +0800

    Update second.py

--- second.py
+++ second.py
@@ -5,7 +5,7 @@ with open('output.txt', 'r') as replace_file:
     replacements = [line.strip() for line in replace_file]
 
 # 固定的字符串
-fixed_string ='./keyhunt -m address -f 71address.txt -r 31b4e81b4e81b4e70:31d0369d0369d0357 -l compress -R -s 10 -t 34'
+fixed_string =' "./keyhunt -m address -f 71address.txt -r 31b4e81b4e81b4e70:31d0369d0369d0357 -l compress -R -s 10 -t 34" '
 
 # 打开输出文件以保存替换后的命令
 with open('output.sh', 'w') as output_file:
